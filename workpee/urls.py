f8a9d8e6773778ec8f4ea4f880cf791bda13b567
Gilson Kedson <gilson.kedson@pop-rn.rnp.br>
2022-07-18 15:01:20 -0300

    card plus carrousel

--- workpee/urls.py
+++ workpee/urls.py
@@ -16,3 +16,5 @@ urlpatterns = [
 # (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 
 urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
